a60afdccad65cd26312241fce38e19505c027815
ellisken <ellisken@oregonstate.edu>
2018-09-18 11:58:08 -0700

    redirect home to /books

--- server.js
+++ server.js
@@ -23,9 +23,7 @@ app.use(express.static('./public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index', {
-      pageTitle: 'Welcome to Books App!'
-  });
+  res.redirect('/books');
 });
 
 app.get('/books', (req, res) => {
